7059d1faa7dbe03838479375ed3c39dadd374ba1
Az zahra <azzahrasitifatimah98@gmail.com>
2023-10-23 10:39:06 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum perspiciatis dolorum doloribus voluptatum. Omnis quo inventore non, voluptatibus tenetur autem.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
