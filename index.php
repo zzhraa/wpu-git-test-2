2dbdc61143aefdd912fe9745dd59b958d9f78f46
Az zahra <azzahrasitifatimah98@gmail.com>
2023-10-23 10:32:58 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum perspiciatis dolorum doloribus voluptatum. Omnis quo inventore non, voluptatibus tenetur autem.</p>
 </body>
 </html>
